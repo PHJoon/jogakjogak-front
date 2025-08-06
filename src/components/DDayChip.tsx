94572d786e1a10a778f9fe127ae49b13284007e8
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2025-08-06 17:43:27 +0900

    fix: 지원분석 상세보기 페이지에서 지원완료 컬러칩 적용되게 수정

--- src/components/DDayChip.tsx
+++ src/components/DDayChip.tsx
@@ -1,45 +1,56 @@
-import React from "react";
-import styles from "./DDayChip.module.css";
+import React from 'react';
+import styles from './DDayChip.module.css';
+import Image from 'next/image';
+import applyIcon from '../assets/images/color_chip/ic_apply.svg';
 
 interface Props {
-  alarm?: "on" | "off";
-  state?: "default" | "alarm";
+  alarm?: 'on' | 'off';
+  state?: 'default' | 'alarm';
   className?: string;
   dDay?: number;
 }
 
 export function DDayChip({
-  state = "default",
-  className = "",
-  dDay = 52
+  state = 'default',
+  className = '',
+  dDay = 52,
 }: Props) {
   switch (className) {
-    case "dDay-dayover":
+    case 'dDay-apply':
       return (
-          <div className={`${styles.chip} ${styles[state]} ${styles[className]}`}>
-            <span className={styles.text}>지원마감</span>
-          </div>
+        <div className={`${styles.chip} ${styles[state]} ${styles[className]}`}>
+          <Image
+            src={applyIcon}
+            alt='apply'
+            width='12'
+            height='12'
+          />
+          <span className={styles.text}>지원완료</span>
+        </div>
       );
-    case "dDay-day0":
+    case 'dDay-dayover':
       return (
-          <div className={`${styles.chip} ${styles[state]} ${styles[className]}`}>
-            <span className={styles.text}>오늘 마감</span>
-          </div>
+        <div className={`${styles.chip} ${styles[state]} ${styles[className]}`}>
+          <span className={styles.text}>지원마감</span>
+        </div>
       );
-    case "dDay-anytime":
+    case 'dDay-day0':
       return (
-          <div className={`${styles.chip} ${styles[state]} ${styles[className]}`}>
-            <span className={styles.text}>상시채용</span>
-          </div>
+        <div className={`${styles.chip} ${styles[state]} ${styles[className]}`}>
+          <span className={styles.text}>오늘 마감</span>
+        </div>
+      );
+    case 'dDay-anytime':
+      return (
+        <div className={`${styles.chip} ${styles[state]} ${styles[className]}`}>
+          <span className={styles.text}>상시채용</span>
+        </div>
       );
     default:
       return (
-          <div className={`${styles.chip} ${styles[state]} ${styles[className]}`}>
-            <span className={styles.text}>D-{dDay}</span>
-          </div>
+        <div className={`${styles.chip} ${styles[state]} ${styles[className]}`}>
+          <span className={styles.text}>D-{dDay}</span>
+        </div>
       );
   }
-
-
-
 }
