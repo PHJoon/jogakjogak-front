f06264839c7c5d16e4490c9a03afb20415beb914
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2025-08-12 11:41:30 +0900

    chore: 프로젝트 기본 패키지 및 환경 설정
    - husky, lint-staged
    - prettier, eslint-plugin-import
    - vscode settings.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import { globalIgnores } from 'eslint/config'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,52 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  globalIgnores(['node_modules/**', 'dist/**', 'build/**', '.next/**', 'out/**', 'coverage/**']),
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:import/recommended',
+    'prettier'
+  ),
+  {
+    settings: {
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
+    },
+
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'type',
+            // Imports of builtins are first
+            'builtin',
+            // Then sibling and parent imports. They can be mingled together
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'object',
+            'unknown'
+          ],
+          pathGroups: [
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      'no-unused-vars': 'off', // TS가 담당하도록 끔
+      '@typescript-eslint/no-unused-vars': 'off', // next/typescript가 관리하는 경우 보통 off
+    },
+  },
 ];
 
 export default eslintConfig;
