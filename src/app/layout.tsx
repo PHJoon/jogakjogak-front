bb45dea0541814b97969843ed9950dfbf36a9e03
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2025-08-18 18:26:57 +0900

    fix: sitemap, robots, head 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,16 +45,16 @@ export const metadata: Metadata = {
     telephone: false,
   },
   alternates: {
-    canonical: 'https://www.jogakjogak.com/',
+    canonical: 'https://jogakjogak.com/',
   },
   openGraph: {
     title: '조각조각',
     description: 'AI와 함께하는 나의 취업 성공 투두리스트',
-    url: 'https://www.jogakjogak.com',
+    url: 'https://jogakjogak.com',
     siteName: '조각조각',
     images: [
       {
-        url: 'https://www.jogakjogak.com/og_image_v1.png',
+        url: 'https://jogakjogak.com/og_image_v1.png',
         width: 1200,
         height: 630,
         alt: '조각조각 OG 이미지',
