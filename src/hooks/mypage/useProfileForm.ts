0d0d63a53136d317b8f79d892d20b7972332bafe
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2025-09-12 15:37:32 +0900

    fix: 프로필 닉네임 폼 규칙 수정

--- src/hooks/mypage/useProfileForm.ts
+++ src/hooks/mypage/useProfileForm.ts
@@ -24,7 +24,7 @@ export default function useProfileForm() {
       maxLength: { value: 12, message: '닉네임은 12자 이내로 입력해주세요.' },
       minLength: { value: 4, message: '닉네임은 4자 이상이어야 합니다.' },
       validate: (value) =>
-        /^[가-힣a-zA-Z0-9]+$/.test(value) ||
+        /^[가-힣a-zA-Z0-9 ]+$/.test(value) ||
         '닉네임에는 한글, 영문, 숫자만 사용할 수 있습니다.',
     }),
     email: register('email'),
