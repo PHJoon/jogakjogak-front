5ecb1209e5d438d2320848794aa2eff29d5780bc
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2025-09-20 15:28:55 +0900

    feat: onboarding 업데이트 route api 추가

---
+++ src/app/api/member/my_page/update_is_onboarded/route.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { ERROR_CODES, ERROR_MESSAGES } from '@/constants/errorCode';
+import { API_BASE_URL } from '@/lib/config';
+
+export async function PATCH(request: NextRequest) {
+  try {
+    const accessToken = request.cookies.get('access_token')?.value ?? null;
+    if (!accessToken) {
+      return NextResponse.json(
+        {
+          errorCode: ERROR_CODES.NO_ACCESS_TOKEN,
+          message: ERROR_MESSAGES.NO_ACCESS_TOKEN,
+        },
+        { status: 401 }
+      );
+    }
+    const body = await request.json();
+
+    // 백엔드 서버로 요청
+    const response = await fetch(
+      `${API_BASE_URL}/member/my-page/update-is-onboarded`,
+      {
+        method: 'PATCH',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      }
+    );
+
+    const data = await response.json();
+    return NextResponse.json(data, { status: response.status });
+  } catch (error) {
+    console.error(
+      'Next server error [PATCH /api/member/my_page/update]: ',
+      error
+    );
+    return NextResponse.json(
+      {
+        errorCode: ERROR_CODES.NEXT_SERVER_ERROR,
+        message: ERROR_MESSAGES.NEXT_SERVER_ERROR,
+      },
+      { status: 500 }
+    );
+  }
+}
